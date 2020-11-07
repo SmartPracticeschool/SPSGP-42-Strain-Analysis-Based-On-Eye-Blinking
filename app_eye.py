24f50a72882e819141e1d54524d91840bca9969a
tans07 <59682959+tans07@users.noreply.github.com>
2020-11-08 00:23:10 +0530

    Add files via upload

---
+++ app_eye.py
@@ -0,0 +1,26 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import datetime
+from gtts import gTTS
+import tkinter as tk
+from tkinter import ttk
+from playsound import playsound
+from scipy.spatial import distance as dist
+print("hello")
+print("hii")
+
+
+# %%
+
+
+
