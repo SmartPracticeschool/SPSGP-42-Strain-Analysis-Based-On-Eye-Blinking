81f8057423c8ac62f2a3a0769417483ba9b1516c
tans07 <59682959+tans07@users.noreply.github.com>
2020-11-08 21:02:09 +0530

    Update app_eye.py

--- app_eye.py
+++ app_eye.py
@@ -1,4 +1,4 @@
-#importing necessary libraries
+#importing necessary libraries(TSK-572)
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -14,6 +14,39 @@ import tkinter as tk
 from tkinter import ttk
 from playsound import playsound
 from scipy.spatial import distance as dist
+
+
+#Defining Necessary Functions(TSK-573)
+def playaudio(text):
+    speech = gTTS(text)
+    print(type(speech))
+    speech.save("../output1.mp3")
+    playsound("../output1.mp3")
+    return
+
+LARGE_FONT = ("Verdana",12)
+NORM_FONT = ("Helvetica",10)
+SMALL_FONT = ("Helvetica",8)
+
+def popupmssg(msg):
+    popup = tk.Tk()
+    popup.wm_title("Urgent")
+    style = ttk.Style(popup)
+    style.theme_use("Classic")
+    style.configure('Test.TLabel',background = 'aqua')
+    label = ttk.Label(popup,txt=msg,style = 'Test.TLabel')
+    label.pack(side='top',fill='x',pady=10)
+    B1 = ttk.Button(popup,text='Okay',command=popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
 
 
 
